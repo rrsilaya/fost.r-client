118fd516551678a644f50dae1ed8c852a027be11
ciaramaegotis <crgotis@up.edu.ph>
2017-07-04 16:40:02 +0800

    vertically adjusted the card

--- src/app/pages/login/components/LoginPanel.js
+++ src/app/pages/login/components/LoginPanel.js
@@ -6,7 +6,7 @@ class LoginPanel extends Component {
       //
       <div className="">
         <div className="uk-flex uk-flex-center uk-flex-middle">
-          <div className="uk-card uk-card-default uk-width-1-3@m">
+          <div className="uk-card uk-card-default uk-width-1-3@m uk-position-center">
             <div className="uk-card-header">
               <div className="uk-grid-small uk-flex-middle" data-uk-grid>
                 <div className="uk-width-auto">
@@ -71,7 +71,6 @@ class LoginPanel extends Component {
 
         </div>
       </div>
-      //
     );
   }
 }
