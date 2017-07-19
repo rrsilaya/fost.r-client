996f19f4aebb5cf4d585ee4cb24cf0c1e434ed97
ciaramaegotis <crgotis@up.edu.ph>
2017-07-19 10:46:13 +0800

    final version

--- src/app/pages/no-page/components/ErrorPanel.js
+++ src/app/pages/no-page/components/ErrorPanel.js
@@ -7,22 +7,14 @@ class NoPage extends Component {
       <div className="uk-text-center uk-position-center">
         <div className="uk-inline-clip uk-transition-toggle uk-light">
           <img className="uk-border-circle" src={image} alt="image" />
-          <div className="uk-position-center">
-            <div className="uk-transition-slide-top-small">
-              <h2 className="uk-margin-remove"><b>You can try: </b></h2>
-            </div>
-            <div className="uk-transition-slide-bottom-small">
-              <h4 className="uk-margin-remove"><b>• Reloading the page</b></h4>
-            </div>
-            <div className="uk-transition-slide-bottom-small">
-              <h4 className="uk-margin-remove">
-                <b>• Choose another route</b>
-              </h4>
-            </div>
-          </div>
         </div>
         <h1 className="uk-margin-small-top">
-          Oops! You have reached 404 page!
+          <b>
+            Oops! You have reached 404 page!
+          </b>
+        </h1>
+        <h1>
+          • Reload the page <br /> • Try another route
         </h1>
       </div>
     );
