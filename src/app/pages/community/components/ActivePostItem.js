d097fd6e0dd20f1259dd58dea7764c2c467c9a3a
rrsilaya <rrsilaya@up.edu.ph>
2017-07-20 21:52:41 +0800

    Made active post to link

--- src/app/pages/community/components/ActivePostItem.js
+++ src/app/pages/community/components/ActivePostItem.js
@@ -1,24 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ActivePostItem = () => {
   return (
-    <li className="uk-grid-match uk-grid-collapse" data-uk-grid>
-      <div className="uk-flex uk-flex-middle uk-margin-right">
-        <div className="uk-text-success uk-text-large">
-          <span data-uk-icon="icon: triangle-up" />&nbsp;5
+    <li>
+      <Link to="/community/post/foo" className="uk-link-reset">
+        <div className="uk-grid-match uk-grid-collapse" data-uk-grid>
+          <div className="uk-flex uk-flex-middle uk-margin-right">
+            <div className="uk-text-success uk-text-large">
+              <span data-uk-icon="icon: triangle-up" />&nbsp;5
+            </div>
+          </div>
+          <div className="uk-width-expand uk-text-small uk-flex uk-flex-middle">
+            <div>
+              <h5 className="uk-text-bold uk-margin-remove">
+                lorem ipsum - dolor sit amet
+              </h5>
+              <p className="uk-margin-remove uk-text-truncate">
+                How to time delay submit button with JQuery in Wordpress Avada
+                Theme?
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="uk-width-expand uk-text-small uk-flex uk-flex-middle">
-        <div>
-          <h5 className="uk-text-bold uk-margin-remove">
-            lorem ipsum - dolor sit amet
-          </h5>
-          <p className="uk-margin-remove uk-text-truncate">
-            How to time delay submit button with JQuery in Wordpress Avada
-            Theme?
-          </p>
-        </div>
-      </div>
+      </Link>
     </li>
   );
 };
