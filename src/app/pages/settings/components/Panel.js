bfe2259d85b8048fdbfbef3f3bfa7957b0619a21
ciaramaegotis <crgotis@up.edu.ph>
2017-07-21 10:32:09 +0800

    initial switcher for settings-page

--- src/app/pages/settings/components/Panel.js
+++ src/app/pages/settings/components/Panel.js
@@ -5,25 +5,21 @@ class Panel extends Component {
   render() {
     return (
       <div>
-        <ul className="uk-tab-right" data-uk-tab>
-          <li className="uk-active">
-            <a href="#">    <span data-uk-icon="icon: user" /> Profile</a>
-          </li>
-          <li>
-            <NavLink to="/settings/privacy">
-              {' '}   <span data-uk-icon="icon: lock" /> Password and Privacy
-            </NavLink>
-          </li>
-          <li>
-            <a href="#">
-              {' '}   <span data-uk-icon="icon: home" /> Muted Shelters
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              {' '}   <span data-uk-icon="icon: push" /> Pending Transactions
-            </a>
-          </li>
+        <ul
+          classname="uk-tab-right"
+          data-uk-tab
+          data-uk-switcher="{connect:'#my-id'}">
+          <li><a href="">Profile</a></li>
+          <li><a href="">Privacy</a></li>
+          <li><a href="">Muted Shelters</a></li>
+          <li><a href="">Transactions</a></li>
+        </ul>
+
+        <ul id="my-id" className="uk-switcher">
+          <li>Content 1</li>
+          <li>Content 2</li>
+          <li>Content 3</li>
+          <li>Content 4</li>
         </ul>
       </div>
     );
@@ -31,3 +27,11 @@ class Panel extends Component {
 }
 
 export default Panel;
+
+<div>
+  <ul class="uk-tab-right" uk-tab>
+    <li class="uk-active"><a href="#">Right</a></li>
+    <li><a href="#">Item</a></li>
+    <li><a href="#">Item</a></li>
+  </ul>
+</div>;
