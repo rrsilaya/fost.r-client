f9e20611b0ac38b11ee2e80ac828bbf2a99f60c5
ciaramaegotis <crgotis@up.edu.ph>
2017-07-21 15:30:15 +0800

    added home icon final version (not final version of home-page)

--- src/app/pages/home/components/WelcomePanel.js
+++ src/app/pages/home/components/WelcomePanel.js
@@ -6,14 +6,21 @@ class WelcomePanel extends Component {
     return (
       <div className="uk-text-center">
         <img
-          className="uk-padding-large uk-padding-remove-bottom uk-thumbnail uk-thumbnail-xsmall"
+          className="uk-padding-medium uk-padding-remove-bottom uk-thumbnail uk-thumbnail-xsmall"
           src={logo}
           alt="logo image"
         />
-        <h1 className="uk-padding-remove"> <b> Welcome to fost.r! </b> </h1>
+        <h1 className="uk-padding-remove">
+          {' '}<b> Adopting animals has never been so easy! </b>{' '}
+        </h1>
         <h3 className="uk-padding-remove-top">
-          {' '}Have a companion with just a click!{' '}
+          <i>
+            {' '} "...Connecting animals and humans since 2017..."{' '}
+          </i>
         </h3>
+        <p uk-margin>
+          {' '}<button className="uk-button uk-button-danger">ADOPT NOW</button>
+        </p>
 
       </div>
     );
