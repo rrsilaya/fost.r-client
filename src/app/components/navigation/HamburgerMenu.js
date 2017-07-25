4b5dbcdf351112da63574520843cc623d110c8e2
rrsilaya <rrsilaya@up.edu.ph>
2017-07-25 16:00:30 +0800

    Added more options in hamburger menu

--- src/app/components/navigation/HamburgerMenu.js
+++ src/app/components/navigation/HamburgerMenu.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 import navpages from './navpages';
 import logo from '../../../assets/images/logo2-dblue.png';
+import options from './components/option-links';
 
 class HamburgerMenu extends Component {
   render() {
@@ -22,6 +23,25 @@ class HamburgerMenu extends Component {
                 </NavLink>
               </li>
             )}
+            <li className="uk-nav-header">More Options</li>
+            <li>
+              <ul className="uk-nav-sub">
+                {options.map((link, key) =>
+                  <li key={key}>
+                    <Link to={link.route} data-uk-toggle="target: #main">
+                      {link.label}
+                    </Link>
+                  </li>
+                )}
+                <li className="uk-nav-divider" />
+                <li>
+                  <span
+                    className="uk-margin-small-right"
+                    data-uk-icon="icon: sign-out; ratio: 0.75"
+                  />Sign out
+                </li>
+              </ul>
+            </li>
           </ul>
         </div>
       </div>
--- src/app/components/navigation/components/MoreOptions.js
+++ src/app/components/navigation/components/MoreOptions.js
@@ -1,45 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import options from './option-links';
+
 const MoreOptions = () => {
   return (
     <div
-      className="uk-text-left uk-padding-medium uk-text-small option-panel"
+      className="uk-text-left uk-padding-medium option-panel"
       data-uk-dropdown="mode: click">
       <ul className="uk-nav uk-dropdown-nav more-options">
         <li className="uk-nav-header">More Options</li>
-        <li>
-          <Link to="/settings">
-            <span
-              className="uk-margin-small-right"
-              data-uk-icon="icon: cog; ratio: 0.75"
-            />Account Settings
-          </Link>
-        </li>
-        <li>
-          <Link to="/rescue">
-            <span
-              className="uk-margin-small-right"
-              data-uk-icon="icon: lifesaver; ratio: 0.75"
-            />Rescue
-          </Link>
-        </li>
-        <li>
-          <Link to="/feedback">
-            <span
-              className="uk-margin-small-right"
-              data-uk-icon="icon: forward; ratio: 0.75"
-            />Feedback
-          </Link>
-        </li>
-        <li>
-          <Link to="/help">
-            <span
-              className="uk-margin-small-right"
-              data-uk-icon="icon: question; ratio: 0.75"
-            />Help
-          </Link>
-        </li>
+        {options.map((link, key) =>
+          <li key={key}>
+            <Link to={link.route}>
+              <span
+                className="uk-margin-small-right"
+                data-uk-icon={`icon: ${link.icon}; ratio: 0.75`}
+              />
+              {link.label}
+            </Link>
+          </li>
+        )}
+
         <li className="uk-nav-divider" />
         <li>
           <span
