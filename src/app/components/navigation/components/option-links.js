59b716543f0512d207c34b1f5d65769a4037016e
rrsilaya <rrsilaya@up.edu.ph>
2017-07-25 16:00:52 +0800

    Added more options in hamburger menu

---
+++ src/app/components/navigation/components/option-links.js
@@ -0,0 +1,24 @@
+const optionLinks = [
+  {
+    route: '/settings',
+    label: 'Account Settings',
+    icon: 'cog'
+  },
+  {
+    route: '/rescue',
+    label: 'Rescue',
+    icon: 'lifesaver'
+  },
+  {
+    route: '/feedback',
+    label: 'Feedback',
+    icon: 'forward'
+  },
+  {
+    route: '/help',
+    label: 'Help',
+    icon: 'question'
+  }
+];
+
+export default optionLinks;
