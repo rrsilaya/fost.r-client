cfd17a12512135a80c2ecf3a2b02ae98bf714669
rrsilaya <rrsilaya@up.edu.ph>
2017-07-31 11:01:48 +0800

    Fixed route for images

--- src/app/pages/admin/components/PetItem.js
+++ src/app/pages/admin/components/PetItem.js
@@ -5,7 +5,7 @@ const PetItem = ({ name, kind, img, birthday, id }) => {
   return (
     <tr>
       <td className="uk-preserve-width">
-        <img src={img} className="uk-border-circle pet-icon" />
+        <img src={`/signup/${img}`} className="uk-border-circle pet-icon" />
       </td>
       <td>{name}</td>
       <td>{kind}</td>
