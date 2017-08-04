9f08e76428a140c060806e43d1fc63d8f277572f
rrsilaya <rrsilaya@up.edu.ph>
2017-08-04 09:42:33 +0800

    Initial home splash screen

--- src/app/pages/home/Home.js
+++ src/app/pages/home/Home.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import DocumentTitle from 'react-document-title';
 import { Link } from 'react-router-dom';
 
+import Navigation from './components/Navigation';
+
 class Home extends Component {
   render() {
     return (
       <DocumentTitle title="fost.r">
         <div>
-          This is home.
-          <Link to="/login">Login</Link>
+          <div
+            className="uk-background-secondary uk-width-1-1 uk-background-cover uk-background-blend-overlay full-splash uk-light"
+            style={{
+              backgroundImage: `url('http://loremflickr.com/g/1080/720/wildlife')`
+            }}>
+            <Navigation />
+            hello
+          </div>
         </div>
       </DocumentTitle>
     );
